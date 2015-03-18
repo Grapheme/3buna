a9e18a75fe6f0c32285e8919d702a24fcc4f1409
sngrl <reserved@mail.ru>
2015-03-18 18:52:48 +0300

    fix

--- app/views/templates/site/footer.blade.php
+++ app/views/templates/site/footer.blade.php
@@ -10,10 +10,10 @@
     <div class="right">
         <ul class="footer-menu">
             <li><a href="{{ URL::route('page', 'billboards') }}">Размещение на щитах</a></li>
-            <li><a href="#">Изготовление наружно рекламы</a></li>
+            <li><a href="#">Изготовление наружной рекламы</a></li>
             <li><a href="#">Интернет-маркетинг</a></li>
             <li><a href="#">Разработка сайтов</a></li>
-            <li><a href="#">О компании</a></li>
+            <li><a href="{{ URL::route('mainpage') }}">О компании</a></li>
             <li><a href="{{ URL::route('page', 'contacts') }}">Контакты</a></li>
         </ul>
     </div>
--- app/views/templates/site/header.blade.php
+++ app/views/templates/site/header.blade.php
@@ -7,10 +7,12 @@
     <div class="holder">
         <ul class="main-menu">
             <li><a href="{{ URL::route('page', 'billboards') }}">Размещение на щитах</a></li>
+            <!--
             <li><a href="#">Изготовление наружной рекламы</a></li>
             <li><a href="#">Интернет-маркетинг</a></li>
             <li><a href="#">Разработка сайтов</a></li>
-            <li><a href="#">О компании</a></li>
+            -->
+            <li><a href="{{ URL::route('mainpage') }}">О компании</a></li>
             <li><a href="{{ URL::route('page', 'contacts') }}">Контакты</a></li>
         </ul>
     </div><a href="{{ URL::route('mainpage') }}" class="logo"><img src="{{ Config::get('site.theme_path') }}/images/logo.png"></a>
